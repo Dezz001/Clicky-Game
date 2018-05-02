8397993fa7e381781573ac8672154754442ae367
Darrin Davis <dezz001@yahoo.com>
2018-05-02 17:56:14 -0400

    Updating

---
+++ my-app/src/components/ClickMe/clickme.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./clickme.css";
+
+
+const ClickMe = props => (
+    <div className="card img-container" onClick={() => handleClick(props)}>
+
+        <img
+        key={props.id}
+        id={props.id}
+        name={props.name}
+        image={props.image}
+        onClick={props.handleClick}
+        />
+    </div>
+    );
+
+export default clickme
---
+++ my-app/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+
+const Jumbotron = () => (
+	
+  <div className="container text-center">
+    <h1 className="display-4">Play My Clicky Game!</h1>
+    <p className="lead">Click an image once to earn points, click an image twice you lose!</p>
+  </div>
+);
+
+export default Jumbotron;
